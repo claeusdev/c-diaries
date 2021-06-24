177d2398c0a52945c1f3427d25b0da7d0c1fca83
Nana Adjei Manu <claeusdev@gmail.com>
2021-06-24 05:03:56 +0000

    variables intro

--- first/main.c
+++ first/main.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+
+/*
+    Author: Nana Adjei Manu
+    Purpose: My very first c program to print my name
+ */
+// Single line comment.
 
 int main(){
-    printf("Hello World\n");
+
+    /*
+        VARIABLES
+    */
+
+    char str[100];
+    int i = 12;
+    float vars = 99.99;
+    double dub = 9.9e4;
+    _Bool mybool; //std 89 representation of boolean
+    bool anotherBool = false; // std 99 rep of boolean
     return 0;
 }
