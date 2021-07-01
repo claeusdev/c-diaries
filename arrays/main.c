d1f2b05732b8330b1ecb6e0892080190e0dc9bbe
Nana Adjei Manu <claeusdev@gmail.com>
2021-07-01 11:09:21 +0000

    operators and arrays'

---
+++ arrays/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+
+int main(){
+
+
+  int grades[10];
+  int count = 10;
+  int sum = 0;
+  float average;
+
+  printf("Please enter your grades to compute your average");
+
+  for(int i = 0; i < count ; ++ i){
+   printf("%2u", i+1);
+   scanf("%d", &grades[i]);
+   sum = sum + grades[i];
+  }
+
+  average = (float)sum/count;
+  
+  printf("Your average is %f", average);
+  return 0;
+}
--- operators/main.c
+++ operators/main.c
@@ -9,8 +9,18 @@ int main(){
     /* --a, evaluated before the expression is run.*/
     printf("res is %d\n", --a); 
     /* a++, evaluated after the expression is run.*/
-    printf("res is %d\n", a++);
+    printf("res is %d\n", a++ );
     printf("final res is %d\n", a);
+
+    //Bitwise
+    unsigned int a = 60;
+    unsigned int b = 13;
+
+    int result = 0;
+
+    result = a & b;
+
+    printf("The result is %d", result);
 
     return 0;
 }
